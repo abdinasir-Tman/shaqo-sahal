af20f182831ae1219332da1cba95ba33e3a430f4
abdinasir-Tman <abdinasirmursal2@gmail.com>
2023-12-02 08:21:08 -0800

    added little bit info

--- app/api/employer/route.ts
+++ app/api/employer/route.ts
@@ -1,8 +1,27 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-export const POST = async(req: NextRequest)=>{
-         if(req.headers.get("content-length") === "0") return NextResponse.json({error: "please insert the body some info"},{status: 400});
+import ValidateEmployer from "./employerSchemaValidator";
+export const POST = async (req: NextRequest) => {
+  if (req.headers.get("content-length") === "0")
+    return NextResponse.json(
+      { error: "please insert the body some info" },
+      { status: 400 }
+    );
 
-         const body = await req.json();
-         const newEmployer = 
-}
+  try {
+    const body = await req.json();
+    const validate = ValidateEmployer.safeParse(body);
+    if (!validate.success) return NextResponse.json({ status: 400 });
+    const newEmployer = prisma.employer.create({
+      data: {
+        companyName: body.companyName,
+        address: body.address,
+        logo: body.url,
+        email: body.email,
+      },
+    });
+    return NextResponse.json(newEmployer, { status: 201 });
+  } catch (error) {
+    console.log("error at register employer ", error);
+  }
+};
