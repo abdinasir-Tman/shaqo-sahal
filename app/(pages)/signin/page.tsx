2d5463a9c61d45a42be5c5bf0025baa6b75c3dd7
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-01-30 10:43:48 -0800

    added scripts postinstall

--- app/(pages)/signin/page.tsx
+++ app/(pages)/signin/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import React from "react";
 import { API } from "@/lib/config";
+import { redirect } from "next/navigation";
 
 const Signin = () => {
+  const { data: session }: any = useSession();
+  if (session) return redirect(API);
   return (
     <div className="bg-main-50 dark:bg-main-950  w-full h-screen flex justify-center items-center">
       <Button
