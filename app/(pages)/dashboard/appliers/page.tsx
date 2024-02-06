33a5c799090e259a2c478a488e81639528b2822b
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-02-06 11:59:38 -0800

    changed dashboard appliers page

--- app/(pages)/dashboard/appliers/page.tsx
+++ app/(pages)/dashboard/appliers/page.tsx
@@ -25,11 +25,11 @@ const Appliers = async ({ searchParams }: any) => {
     <div className="max-w-4xl mx-auto p-8">
       <div className="mb-6">
         <h1 className="text-2xl dark:text-gray-100 font-bold mb-2">
-          Job Title: {data.title}
+          Job Title: {data?.title}
         </h1>
-        <p className="text-lg text-gray-600">Category: {data.jobCategory}</p>
+        <p className="text-lg text-gray-600">Category: {data?.jobCategory}</p>
         <p className="text-md mt-4 dark:text-gray-100">
-          Description <br /> {data.description}
+          Description <br /> {data?.description}
         </p>
       </div>
       <h1 className="font-bold mb-2 text-2xl font-sans">Appliers</h1>
