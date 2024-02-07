537878291b5b160782135d42e98c08da078b5dce
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-02-07 00:52:02 -0800

    modified image src of custom not found page

--- app/components/NotFound.tsx
+++ app/components/NotFound.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const CustomNotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800">
-      <Image src={"./notfound.svg"} alt="Not Found" width={200} height={200} />
+      <Image src={"/notfound.svg"} alt="Not Found" width={200} height={200} />
       <h1 className="mt-2 text-3xl font-semibold">Page Not Found</h1>
       <p className="mt-2">
         Sorry, we couldn't find the page you're looking for.
