8f9e09f91bfec6e705da3ab79482a6b1118be6b1
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-02-07 23:35:17 -0800

    added custom loading

--- app/(pages)/jobSeeker/loading.tsx
+++ app/(pages)/jobSeeker/loading.tsx
@@ -1,7 +1,15 @@
+import { Loader2 } from "lucide-react";
 import React from "react";
 
 const Loading = () => {
-  return <div>loading.....</div>;
+  return (
+    <div>
+      <div className="h-screen w-full flex justify-center items-center">
+        <Loader2 className="animate-spin md:h-24 md:w-24  mx-2" />
+      </div>
+      .
+    </div>
+  );
 };
 
 export default Loading;
