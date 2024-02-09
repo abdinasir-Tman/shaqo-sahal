f6c4101c9aa90d278858f6fcded4b614c5b9b677
abdinasir-Tman <abdinasirmursal2@gmail.com>
2024-02-09 10:41:02 -0800

    changed to dynamic class application status

--- app/(pages)/dashboard/application/_components/Preview.tsx
+++ app/(pages)/dashboard/application/_components/Preview.tsx
@@ -27,62 +27,68 @@ const Preview = async () => {
   } catch (error) {
     console.log(error);
   }
-  if (data?.length <= 3)
-    return (
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
-        <div className="p-3 border-b-4 rounded-md bg-gray-100 dark:bg-gray-800 border-green-800">
-          <h1>Pending</h1>
-          <div className="flex justify-between items-center">
-            <h1 className="text-4xl font-sans font-bold">
-              {data[0]?._count.status ? data[0]?._count.status : 0}
-            </h1>
-            <RiPassPendingFill className="text-7xl text-green-800" />
-          </div>
-        </div>
+  // if (data?.length <= 3)
+  //   return (
+  //     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
+  //       <div className="p-3 border-b-4 rounded-md bg-gray-100 dark:bg-gray-800 border-green-800">
+  //         <h1>Pending</h1>
+  //         <div className="flex justify-between items-center">
+  //           <h1 className="text-4xl font-sans font-bold">
+  //             {data[0]?._count.status ? data[0]?._count.status : 0}
+  //           </h1>
+  //           <RiPassPendingFill className="text-7xl text-green-800" />
+  //         </div>
+  //       </div>
 
-        {/* /////////////  */}
-        <div className="p-3 border-b-4 rounded-md bg-gray-100 dark:bg-gray-800 border-blue-800">
-          <h1>Interview</h1>
-          <div className="flex justify-between items-center">
-            <h1 className="text-4xl font-sans font-bold">
-              {data[1]?._count.status ? data[1]?._count.status : 0}
-            </h1>
-            <AiFillSchedule className="text-7xl text-blue-800" />
-          </div>
-        </div>
-        {/* /////////////  */}
-        <div className="p-3 border-b-4 rounded-md bg-gray-100 dark:bg-gray-800 border-red-800">
-          <h1>Declained</h1>
-          <div className="flex justify-between items-center">
-            <h1 className="text-4xl font-sans font-bold">
-              {data[2]?._count.status ? data[2]?._count.status : 0}
-            </h1>
-            <FaLock className="text-7xl text-red-800" />
-          </div>
-        </div>
-      </div>
-    );
+  //       {/* /////////////  */}
+  //       <div className="p-3 border-b-4 rounded-md bg-gray-100 dark:bg-gray-800 border-blue-800">
+  //         <h1>Interview</h1>
+  //         <div className="flex justify-between items-center">
+  //           <h1 className="text-4xl font-sans font-bold">
+  //             {data[1]?._count.status ? data[1]?._count.status : 0}
+  //           </h1>
+  //           <AiFillSchedule className="text-7xl text-blue-800" />
+  //         </div>
+  //       </div>
+  //       {/* /////////////  */}
+  //       <div className="p-3 border-b-4 rounded-md bg-gray-100 dark:bg-gray-800 border-red-800">
+  //         <h1>Declained</h1>
+  //         <div className="flex justify-between items-center">
+  //           <h1 className="text-4xl font-sans font-bold">
+  //             {data[2]?._count.status ? data[2]?._count.status : 0}
+  //           </h1>
+  //           <FaLock className="text-7xl text-red-800" />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
       {data?.map((app: any) => (
         <div
           key={app.id}
-          className={cn(
-            "p-3 border-b-3 rounded-md bg-gray-100 dark:bg-gray-800",
-            app?.status === "pending"
+          className={`p-3 border-b-4 rounded-md bg-gray-100 dark:bg-gray-800 ${
+            app?.status == "pending"
               ? "border-b-green-700"
-              : app?.status === "interview"
+              : app?.status == "interview"
               ? "border-b-blue-800"
               : "border-b-red-800"
-          )}
+          }`}
         >
           {" "}
           <h1>{app?.status}</h1>
           <div className="flex justify-between items-center">
             <h1 className="text-4xl font-sans font-bold">
-              {app?._count.status}
+              {app?._count.status ? app?._count.status : 0}
             </h1>
-            <RiPassPendingFill className="text-7xl text-green-800" />
+            {app?.status == "pending" ? (
+              <RiPassPendingFill className="text-7xl text-green-800" />
+            ) : app?.status == "interview" ? (
+              <AiFillSchedule className="text-7xl text-blue-800" />
+            ) : (
+              <FaLock className="text-7xl text-red-800" />
+            )}
           </div>
         </div>
       ))}
